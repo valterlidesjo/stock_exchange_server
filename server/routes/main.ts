cea04454ccedf6521fbbadb4e7718e61c1dc8c5c
valterlidesjo <valle.lidesjo@gmail.com>
2024-12-03 12:35:33 +0100

    🔥

--- server/routes/main.ts
+++ server/routes/main.ts
@@ -162,3 +162,19 @@ router.get("/savedTickers", authMiddleware, async (req: any, res: any) => {
   }
 });
 
+router.get("/userInfo", authMiddleware, async (req: any, res: any) => {
+  try {
+    const userId = req.userId;
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({ username: user.userName, email: user.email, created: user.createdAt });
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
